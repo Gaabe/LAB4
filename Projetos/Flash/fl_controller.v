fdd15832b9899d30a8edd4ebb3d6162e1952ab7e
leonleao <leon.leao92@gmail.com>
2016-08-08 23:51:51 -0300

    Update fl_controller.v

--- Projetos/Flash/fl_controller.v
+++ Projetos/Flash/fl_controller.v
@@ -1,17 +1,17 @@
 
-// Created by fizzim.pl version 5.10 on 2016:07:27 at 18:40:30 (www.fizzim.com)
+// Created by fizzim.pl version 5.10 on 2016:07:30 at 23:49:39 (www.fizzim.com)
 
 module fl_controller (
   output reg [22:0] FL_ADDR,
   output wire FL_CE_N,
   output wire FL_OE_N,
-  output wire FL_RST_T,
+  output wire FL_RST_N,
   output wire FL_WE_N,
-  output reg [15:0] data_out0,
-  output reg [15:0] data_out1,
   output wire finished,
   output wire ready,
-  input wire CLOCK_50,
+  output wire [15:0] data_out0,
+  output wire [15:0] data_out1,
+  input wire clk_25,
   input wire [7:0] FL_DQ,
   input wire ack,
   input wire rst_n,
@@ -20,88 +20,78 @@ module fl_controller (
 
   // state bits
   parameter 
-  IDLE          = 10'b0000011100, // extra=0000 ready=0 finished=1 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Addressing    = 10'b0000001100, // extra=0000 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Buffering0    = 10'b0001001100, // extra=0001 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Buffering1    = 10'b0010001100, // extra=0010 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Getting_ready = 10'b0011001100, // extra=0011 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Pre_Read      = 10'b0100001100, // extra=0100 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Reading0      = 10'b0101001100, // extra=0101 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Reading1      = 10'b0110001100, // extra=0110 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Reading2      = 10'b0111001100, // extra=0111 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Reading3      = 10'b1000001100, // extra=1000 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Reseting      = 10'b1001001100, // extra=1001 ready=0 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
-  Sending       = 10'b0000101100; // extra=0000 ready=1 finished=0 FL_WE_N=1 FL_RST_T=1 FL_OE_N=0 FL_CE_N=0 
+  IDLE          = 4'b0000, // ready=0 finished=1 FL_WE_N=1 FL_RST_N=1 FL_OE_N=1 FL_CE_N=1 
+  Addressing    = 4'b0001, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Getting_ready = 4'b0010, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=1 FL_CE_N=1 
+  Pre_Read      = 4'b0011, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Reading0      = 4'b0100, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Reading1      = 4'b0101, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Reading2      = 4'b0110, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Reseting      = 4'b0111, // ready=0 finished=0 FL_WE_N=1 FL_RST_N=0 FL_OE_N=1 FL_CE_N=1 
+  Sending       = 4'b1000, // ready=1 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
+  Stabilizing   = 4'b1001; // ready=0 finished=0 FL_WE_N=1 FL_RST_N=1 FL_OE_N=0 FL_CE_N=0 
 
-  reg [9:0] state;
-  reg [9:0] nextstate;
+  reg [3:0] state;
+  reg [3:0] nextstate;
   reg buffer;
   reg buffer_count;
-  reg [4:0] wait_count;
+  reg [15:0] current_data0;
+  reg [15:0] current_data1;
+  reg [3:0] wait_count;
   reg [22:0] next_FL_ADDR;
   reg next_buffer;
   reg next_buffer_count;
-  reg [15:0] next_data_out0;
-  reg [15:0] next_data_out1;
-  reg [4:0] next_wait_count;
+  reg [15:0] next_current_data0;
+  reg [15:0] next_current_data1;
+  reg [3:0] next_wait_count;
 
   // comb always block
   always @* begin
-    nextstate = 10'bxxxxxxxxxx; // default to x because default_state_is_x is set
+    nextstate = 4'bxxxx; // default to x because default_state_is_x is set
     next_FL_ADDR[22:0] = FL_ADDR[22:0];
     next_buffer = buffer;
     next_buffer_count = buffer_count;
-    next_data_out0[15:0] = data_out0[15:0];
-    next_data_out1[15:0] = data_out1[15:0];
-    next_wait_count[4:0] = wait_count[4:0];
+    next_current_data0[15:0] = current_data0[15:0];
+    next_current_data1[15:0] = current_data1[15:0];
+    next_wait_count[3:0] = wait_count[3:0];
     case (state)
       IDLE         : begin
+        next_FL_ADDR[22:0] = 0;
         if (start) begin
           nextstate = Reseting;
-          next_wait_count[4:0] = 0;
+          next_wait_count[3:0] = 0;
         end
         else begin
           nextstate = IDLE;
         end
       end
       Addressing   : begin
+        next_FL_ADDR[22:0] = FL_ADDR[22:0] + 1'b1;
         begin
           nextstate = Pre_Read;
-        end
-      end
-      Buffering0   : begin
-        next_data_out0[15:0] = data_out0[15:0] << 8;
-        begin
-          nextstate = Pre_Read;
-          next_buffer_count = 1;
-          next_wait_count[4:0] = 1;
-        end
-      end
-      Buffering1   : begin
-        next_data_out1[15:0] = data_out1[15:0] << 8;
-        begin
-          nextstate = Pre_Read;
+          next_wait_count[3:0] = 0;
         end
       end
       Getting_ready: begin
-        if (wait_count[4:0] < 2) begin
+        if (wait_count[3:0] < 1) begin
           nextstate = Getting_ready;
-          next_wait_count[4:0] = wait_count[4:0] + 1'b1;
+          next_wait_count[3:0] = wait_count[3:0] +1'b1;
         end
         else begin
           nextstate = Pre_Read;
           next_buffer_count = 0;
-          next_wait_count[4:0] = 0;
+          next_wait_count[3:0] = 0;
           next_buffer = 0;
         end
       end
       Pre_Read     : begin
-        if (wait_count[4:0]<5) begin
+        if (wait_count[3:0] < 2) begin
           nextstate = Pre_Read;
-          next_wait_count[4:0] = wait_count[4:0] +1'b1;
+          next_wait_count[3:0] = wait_count[3:0] + 1'b1;
         end
         else if (!buffer && !buffer_count) begin
           nextstate = Reading0;
+          next_wait_count[3:0] = 0;
         end
         else if (!buffer && buffer_count) begin
           nextstate = Reading1;
@@ -109,95 +99,92 @@ module fl_controller (
         else if (buffer && !buffer_count) begin
           nextstate = Reading2;
         end
-        else begin
-          nextstate = Reading3;
-        end
       end
       Reading0     : begin
-        next_data_out0[15:0] = fl_dq;
+        next_current_data0[15:0] = FL_DQ[7:0] << 8;
         begin
-          nextstate = Buffering0;
+          nextstate = Addressing;
+          next_buffer_count = 1;
         end
       end
       Reading1     : begin
-        next_data_out0[15:0] = data_out0[15:0] + fl_dq;
+        next_current_data0[15:0] = current_data0[15:0] + FL_DQ[7:0];
         begin
           nextstate = Addressing;
           next_buffer_count = 0;
-          next_wait_count[4:0] = 1;
           next_buffer = 1;
         end
       end
       Reading2     : begin
-        next_data_out1[15:0] = fl_dq;
+        next_current_data1[15:0] = FL_DQ[7:0] << 8;
         begin
-          nextstate = Buffering1;
-          next_wait_count[4:0] = 1;
-          next_buffer_count = 1;
-        end
-      end
-      Reading3     : begin
-        next_data_out1[15:0] = data_out1[15:0] + fl_dq;
-        begin
-          nextstate = Sending;
+          nextstate = Stabilizing;
         end
       end
       Reseting     : begin
-        if (wait_count[4:0] < 25) begin
+        if (wait_count[3:0] < 12) begin
           nextstate = Reseting;
-          next_wait_count[4:0] = wait_count[4:0]+1'b1;
+          next_wait_count[3:0] = wait_count[3:0] + 1'b1;
         end
         else begin
           nextstate = Getting_ready;
-          next_wait_count[4:0] = 0;
+          next_wait_count[3:0] = 0;
         end
       end
       Sending      : begin
-        if (ack && fl_addr[22:0] == 8294399) begin
+        if (ack && FL_ADDR[22:0] == 8294399) begin
           nextstate = IDLE;
         end
         else if (ack) begin
           nextstate = Addressing;
           next_buffer_count = 0;
-          next_wait_count[4:0] = 1;
           next_buffer = 0;
         end
         else begin
           nextstate = Sending;
         end
       end
+      Stabilizing  : begin
+        begin
+          nextstate = Sending;
+        end
+      end
+      default : begin
+        nextstate = IDLE; // Added because undefined_states_go_here is set
+      end
     endcase
   end
 
   // Assign reg'd outputs to state bits
-  assign FL_CE_N = state[0];
-  assign FL_OE_N = state[1];
-  assign FL_RST_T = state[2];
-  assign FL_WE_N = state[3];
-  assign finished = state[4];
-  assign ready = state[5];
+  assign FL_CE_N = (state == IDLE || state == Reseting || state == Getting_ready) ? 1'b1 : 1'b0;
+  assign FL_OE_N = (state == IDLE || state == Reseting || state == Getting_ready) ? 1'b1 : 1'b0;
+  assign FL_RST_N = (state == Reseting) ? 1'b0 : 1'b1;
+  assign FL_WE_N = 1'b1;
+  assign finished = (state == IDLE) ? 1'b1 : 1'b0;
+  assign ready = (state == Sending) ? 1'b1 : 1'b0;
+  assign data_out0 = (state != IDLE) ? current_data0[15:0] : 16'bz;
+  assign data_out1 = (state != IDLE) ? current_data1[15:0] : 16'bz;
 
   // sequential always block
-  always @(posedge CLOCK_50) begin
+  always @(posedge clk_25) begin
     if (!rst_n) begin
       state <= IDLE;
       FL_ADDR[22:0] <= 0;
       buffer <= 0;
       buffer_count <= 0;
-      data_out0[15:0] <= 0;
-      data_out1[15:0] <= 0;
-      wait_count[4:0] <= 0;
+      current_data0[15:0] <= 0;
+      current_data1[15:0] <= 0;
+      wait_count[3:0] <= 0;
       end
     else begin
       state <= nextstate;
       FL_ADDR[22:0] <= next_FL_ADDR[22:0];
       buffer <= next_buffer;
       buffer_count <= next_buffer_count;
-      data_out0[15:0] <= next_data_out0[15:0];
-      data_out1[15:0] <= next_data_out1[15:0];
-      wait_count[4:0] <= next_wait_count[4:0];
+      current_data0[15:0] <= next_current_data0[15:0];
+      current_data1[15:0] <= next_current_data1[15:0];
+      wait_count[3:0] <= next_wait_count[3:0];
       end
   end
 
 endmodule
-
